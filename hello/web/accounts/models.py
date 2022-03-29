2a1ed68c132304db0d50129c1ec978b35f67c8a4
Nikoleta Zlateva <87802876+Nikoletazl@users.noreply.github.com>
2022-03-29 21:35:17 +0300

    fixed the files

---
+++ hello/web/accounts/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth import models as auth_models
+from django.db import models
+
+
+# Create your models here.
+from hello.web.accounts.managers import PetstagramUserManager
+
+
+class PetstagramUser(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
+    USERNAME_MAX_LENGTH = 25
+
+    username = models.CharField(
+        max_length=USERNAME_MAX_LENGTH,
+        unique=True,
+    )
+
+    date_joined = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    is_staff = models.BooleanField(
+        default=False,
+    )
+
+    USERNAME_FIELD = 'username'
+
+    objects = PetstagramUserManager()
